04776be5ba6e8dc99d9e6cbffaaa67d51aef1a1c
Gustavofh <gustavofh94@gmail.com>
2025-06-03 00:15:07 -0300

    ajustes

--- app/core/database.py
+++ app/core/database.py
@@ -11,8 +11,14 @@ DB_HOST = os.getenv("DB_HOST", "database")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "embrapa")
 
+# DATABASE_URL = (
+#     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# )
+
 DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql://dados_embrapa_user:sRPVK1erV0uFYX0QXBhqC7pDWTpIprZ1"
+    f"@dpg-d0ua9aidbo4c73ap5vng-a.oregon-postgres.render.com:5432/dados_embrapa"
+    "?sslmode=require"
 )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
