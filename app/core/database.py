0cc8242440cbbc534ed32207e27cd690c9c548fa
Gustavofh <gustavofh94@gmail.com>
2025-06-03 00:41:44 -0300

    ajustes

--- app/core/database.py
+++ app/core/database.py
@@ -5,22 +5,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER", "user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-DB_HOST = os.getenv("DB_HOST", "database")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "embrapa")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-# DATABASE_URL = (
-#     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# )
 
 DATABASE_URL = (
-    f"postgresql://dados_embrapa_user:sRPVK1erV0uFYX0QXBhqC7pDWTpIprZ1"
-    f"@dpg-d0ua9aidbo4c73ap5vng-a.oregon-postgres.render.com:5432/dados_embrapa"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     "?sslmode=require"
 )
 
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
